46d12ad6f3533cd6577898c7bbbff89aae8bac5f
wxxsfxyzm <wxxsfxyzm@126.com>
2025-05-27 15:43:32 +0800

    Refactor InstallPrepareDialog to improve readability and add 'Select PKG' string resource

--- app/src/main/java/com/rosan/installer/ui/page/installer/dialog/inner/InstallPrepareDialog.kt
+++ app/src/main/java/com/rosan/installer/ui/page/installer/dialog/inner/InstallPrepareDialog.kt
@@ -12,8 +12,18 @@ import androidx.compose.material.icons.twotone.BugReport
 import androidx.compose.material.icons.twotone.Delete
 import androidx.compose.material.icons.twotone.Info
 import androidx.compose.material.icons.twotone.People
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -22,7 +32,11 @@ import androidx.compose.ui.unit.dp
 import com.rosan.installer.R
 import com.rosan.installer.data.app.util.sortedBest
 import com.rosan.installer.data.installer.repo.InstallerRepo
-import com.rosan.installer.ui.page.installer.dialog.*
+import com.rosan.installer.ui.page.installer.dialog.DialogInnerParams
+import com.rosan.installer.ui.page.installer.dialog.DialogParams
+import com.rosan.installer.ui.page.installer.dialog.DialogParamsType
+import com.rosan.installer.ui.page.installer.dialog.DialogViewAction
+import com.rosan.installer.ui.page.installer.dialog.DialogViewModel
 
 // Assume pausingIcon is accessible
 
@@ -30,50 +44,52 @@ import com.rosan.installer.ui.page.installer.dialog.*
 private fun InstallPrepareEmptyDialog(
     installer: InstallerRepo, viewModel: DialogViewModel
 ): DialogParams {
-    return DialogParams(icon = DialogInnerParams(
-        DialogParamsType.IconPausing.id, pausingIcon
-    ), title = DialogInnerParams(
-        DialogParamsType.InstallerPrepare.id,
-    ) {
-        Text(stringResource(R.string.installer_prepare_install))
-    }, text = DialogInnerParams(
-        DialogParamsType.InstallerPrepareEmpty.id
-    ) {
-        Text(stringResource(R.string.installer_prepare_install_empty))
-    }, buttons = DialogButtons(
-        DialogParamsType.ButtonsCancel.id
-    ) {
-        listOf(DialogButton(stringResource(R.string.previous)) {
-            viewModel.dispatch(DialogViewAction.InstallChoice)
-        }, DialogButton(stringResource(R.string.cancel)) {
-            viewModel.dispatch(DialogViewAction.Close)
+    return DialogParams(
+        icon = DialogInnerParams(
+            DialogParamsType.IconPausing.id, pausingIcon
+        ), title = DialogInnerParams(
+            DialogParamsType.InstallerPrepare.id,
+        ) {
+            Text(stringResource(R.string.installer_prepare_install))
+        }, text = DialogInnerParams(
+            DialogParamsType.InstallerPrepareEmpty.id
+        ) {
+            Text(stringResource(R.string.installer_prepare_install_empty))
+        }, buttons = DialogButtons(
+            DialogParamsType.ButtonsCancel.id
+        ) {
+            listOf(DialogButton(stringResource(R.string.previous)) {
+                viewModel.dispatch(DialogViewAction.InstallChoice)
+            }, DialogButton(stringResource(R.string.cancel)) {
+                viewModel.dispatch(DialogViewAction.Close)
+            })
         })
-    })
 }
 
 @Composable
 private fun InstallPrepareTooManyDialog(
     installer: InstallerRepo, viewModel: DialogViewModel
 ): DialogParams {
-    return DialogParams(icon = DialogInnerParams(
-        DialogParamsType.IconPausing.id, pausingIcon
-    ), title = DialogInnerParams(
-        DialogParamsType.InstallerPrepare.id,
-    ) {
-        Text(stringResource(R.string.installer_prepare_install))
-    }, text = DialogInnerParams(
-        DialogParamsType.InstallerPrepareTooMany.id
-    ) {
-        Text(stringResource(R.string.installer_prepare_install_too_many))
-    }, buttons = DialogButtons(
-        DialogParamsType.ButtonsCancel.id
-    ) {
-        listOf(DialogButton(stringResource(R.string.previous)) {
-            viewModel.dispatch(DialogViewAction.InstallChoice)
-        }, DialogButton(stringResource(R.string.cancel)) {
-            viewModel.dispatch(DialogViewAction.Close)
+    return DialogParams(
+        icon = DialogInnerParams(
+            DialogParamsType.IconPausing.id, pausingIcon
+        ), title = DialogInnerParams(
+            DialogParamsType.InstallerPrepare.id,
+        ) {
+            Text(stringResource(R.string.installer_prepare_install))
+        }, text = DialogInnerParams(
+            DialogParamsType.InstallerPrepareTooMany.id
+        ) {
+            Text(stringResource(R.string.installer_prepare_install_too_many))
+        }, buttons = DialogButtons(
+            DialogParamsType.ButtonsCancel.id
+        ) {
+            listOf(DialogButton(stringResource(R.string.previous)) {
+                viewModel.dispatch(DialogViewAction.InstallChoice)
+            }, DialogButton(stringResource(R.string.cancel)) {
+                viewModel.dispatch(DialogViewAction.Close)
+            })
         })
-    })
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -116,8 +132,10 @@ fun installPrepareDialog( // 小写开头
     LaunchedEffect(forAllUser, allowTestOnly, allowDowngrade, autoDelete, displaySdk) {
         val currentConfig = installer.config
         if (currentConfig.forAllUser != forAllUser) installer.config.forAllUser = forAllUser
-        if (currentConfig.allowTestOnly != allowTestOnly) installer.config.allowTestOnly = allowTestOnly
-        if (currentConfig.allowDowngrade != allowDowngrade) installer.config.allowDowngrade = allowDowngrade
+        if (currentConfig.allowTestOnly != allowTestOnly) installer.config.allowTestOnly =
+            allowTestOnly
+        if (currentConfig.allowDowngrade != allowDowngrade) installer.config.allowDowngrade =
+            allowDowngrade
         if (currentConfig.autoDelete != autoDelete) installer.config.autoDelete = autoDelete
         if (currentConfig.displaySdk != displaySdk) installer.config.displaySdk = displaySdk
     }
@@ -144,11 +162,36 @@ fun installPrepareDialog( // 小写开头
                     FlowRow(
                         horizontalArrangement = Arrangement.spacedBy(8.dp)
                     ) {
-                        Chip(selected = forAllUser, onClick = { forAllUser = !forAllUser }, label = stringResource(id = R.string.config_for_all_user), icon = Icons.TwoTone.People)
-                        Chip(selected = allowTestOnly, onClick = { allowTestOnly = !allowTestOnly }, label = stringResource(id = R.string.config_allow_test_only), icon = Icons.TwoTone.BugReport)
-                        Chip(selected = allowDowngrade, onClick = { allowDowngrade = !allowDowngrade }, label = stringResource(id = R.string.config_allow_downgrade), icon = Icons.AutoMirrored.TwoTone.TrendingDown)
-                        Chip(selected = autoDelete, onClick = { autoDelete = !autoDelete }, label = stringResource(id = R.string.config_auto_delete), icon = Icons.TwoTone.Delete)
-                        Chip(selected = displaySdk, onClick = { displaySdk = !displaySdk }, label = stringResource(id = R.string.config_display_sdk_version), icon = Icons.TwoTone.Info)
+                        Chip(
+                            selected = forAllUser,
+                            onClick = { forAllUser = !forAllUser },
+                            label = stringResource(id = R.string.config_for_all_user),
+                            icon = Icons.TwoTone.People
+                        )
+                        Chip(
+                            selected = allowTestOnly,
+                            onClick = { allowTestOnly = !allowTestOnly },
+                            label = stringResource(id = R.string.config_allow_test_only),
+                            icon = Icons.TwoTone.BugReport
+                        )
+                        Chip(
+                            selected = allowDowngrade,
+                            onClick = { allowDowngrade = !allowDowngrade },
+                            label = stringResource(id = R.string.config_allow_downgrade),
+                            icon = Icons.AutoMirrored.TwoTone.TrendingDown
+                        )
+                        Chip(
+                            selected = autoDelete,
+                            onClick = { autoDelete = !autoDelete },
+                            label = stringResource(id = R.string.config_auto_delete),
+                            icon = Icons.TwoTone.Delete
+                        )
+                        Chip(
+                            selected = displaySdk,
+                            onClick = { displaySdk = !displaySdk },
+                            label = stringResource(id = R.string.config_display_sdk_version),
+                            icon = Icons.TwoTone.Info
+                        )
                     }
                 }
             }
@@ -158,8 +201,16 @@ fun installPrepareDialog( // 小写开头
         ) {
             listOf(
                 DialogButton(stringResource(R.string.install)) { viewModel.dispatch(DialogViewAction.Install) },
-                DialogButton(stringResource(R.string.previous), 2f) { viewModel.dispatch(DialogViewAction.InstallChoice) },
-                DialogButton(stringResource(R.string.cancel), 1f) { viewModel.dispatch(DialogViewAction.Close) }
+                DialogButton(stringResource(R.string.install_choice), 2f) {
+                    viewModel.dispatch(
+                        DialogViewAction.InstallChoice
+                    )
+                },
+                DialogButton(stringResource(R.string.cancel), 1f) {
+                    viewModel.dispatch(
+                        DialogViewAction.Close
+                    )
+                }
             )
         }
     )
