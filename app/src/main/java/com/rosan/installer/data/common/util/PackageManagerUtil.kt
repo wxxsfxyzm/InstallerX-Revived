a4055e59494570c8fe1cb164843c5e2e18be793a
wxxsfxyzm <65166044+wxxsfxyzm@users.noreply.github.com>
2025-06-07 13:01:32 +0800

    Add compatibility for getInstalledPackages method based on SDK version, fix #21

--- app/src/main/java/com/rosan/installer/data/common/util/PackageManagerUtil.kt
+++ app/src/main/java/com/rosan/installer/data/common/util/PackageManagerUtil.kt
@@ -2,10 +2,19 @@ package com.rosan.installer.data.common.util
 
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.core.content.pm.PackageInfoCompat
 
 val PackageInfo.compatVersionCode: Long
     get() = PackageInfoCompat.getLongVersionCode(this)
 
-fun PackageManager.getCompatInstalledPackages(flags: Int): List<PackageInfo> =
-    getInstalledPackages(PackageManager.PackageInfoFlags.of(flags.toLong()))
+// Compat function to run below SDK 33
+fun PackageManager.getCompatInstalledPackages(flags: Int): List<PackageInfo> {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        // For Android 13 (API 33) and above
+        getInstalledPackages(PackageManager.PackageInfoFlags.of(flags.toLong()))
+    } else {
+        // For older versions
+        getInstalledPackages(flags)
+    }
+}
