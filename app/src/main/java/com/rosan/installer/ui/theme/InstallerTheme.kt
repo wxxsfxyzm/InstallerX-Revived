8cd1a3588fba42b017d697f42699cc248c7d73bd
wxxsfxyzm <65166044+wxxsfxyzm@users.noreply.github.com>
2025-07-08 00:10:43 +0800

    feat(theme): enable Expressive motion scheme

--- app/src/main/java/com/rosan/installer/ui/theme/InstallerTheme.kt
+++ app/src/main/java/com/rosan/installer/ui/theme/InstallerTheme.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialExpressiveTheme
+import androidx.compose.material3.MotionScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -112,6 +113,7 @@ fun InstallerTheme(
 
     MaterialExpressiveTheme(
         colorScheme = colorScheme,
+        motionScheme = MotionScheme.expressive(),
         typography = Typography,
         content = content
     )
