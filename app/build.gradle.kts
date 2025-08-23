e1199696dfa3e66ebde53a78722bd6378d94846c
wxxsfxyzm <65166044+wxxsfxyzm@users.noreply.github.com>
2025-08-23 22:50:16 +0800

    chore(deps): refactor Koin dependency declaration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -207,7 +207,7 @@ dependencies {
 
     implementation(libs.lsposed.hiddenapibypass)
 
-    implementation(project.dependencies.platform(libs.koin.bom))
+    implementation(platform(libs.koin.bom))
     implementation(libs.koin.core)
     implementation(libs.koin.android)
     implementation(libs.koin.compose)
