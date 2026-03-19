f4b07909e55df827ca2ff3e384b9b8e57c3aea64
wxxsfxyzm <65166044+wxxsfxyzm@users.noreply.github.com>
2026-03-20 00:27:51 +0800

    style: remove duplicate license header in AnalyzeInstallStateUseCase
    - Remove redundant SPDX license and copyright comment block.

--- app/src/main/java/com/rosan/installer/domain/engine/usecase/AnalyzeInstallStateUseCase.kt
+++ app/src/main/java/com/rosan/installer/domain/engine/usecase/AnalyzeInstallStateUseCase.kt
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-only
 // Copyright (C) 2025-2026 InstallerX Revived contributors
-// SPDX-License-Identifier: GPL-3.0-only
-// Copyright (C) 2025-2026 InstallerX Revived contributors
 package com.rosan.installer.domain.engine.usecase
 
 import com.rosan.installer.core.env.DeviceConfig
