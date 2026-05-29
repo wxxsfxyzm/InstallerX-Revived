4c8ffa9abe1cfa11ab256e1d0db902d680cff313
wxxsfxyzm <65166044+wxxsfxyzm@users.noreply.github.com>
2026-05-29 13:50:36 +0800

    chore: bump version code to 53

--- build-plugins/src/main/kotlin/BuildConfig.kt
+++ build-plugins/src/main/kotlin/BuildConfig.kt
@@ -9,7 +9,7 @@ object BuildConfig {
     const val MIN_SDK = 26
     const val JDK_VERSION = 25
 
-    const val VERSION_CODE = 52
+    const val VERSION_CODE = 53
 }
 
 // Get git commit hash safely, compatible with configuration cache
